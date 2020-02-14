6932388e8b42ec3dbc82e2e4d7878bbe1add8329
Shemy Gan <xuemei.gan@canada.ca>
2020-02-14 11:29:32 -0500

    Support_18671_Add_description_to_managed_attributes,Updated crud test

--- src/test/java/ca/gc/aafc/objectstore/api/crud/ManagedAttributeCRUDIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/crud/ManagedAttributeCRUDIT.java
@@ -5,15 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.Before;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ManagedAttributeFactory;
 
 public class ManagedAttributeCRUDIT extends BaseEntityCRUDIT {
-
+  
+  private JsonNode jsonNode;  
+   
   private ManagedAttribute managedAttributeUnderTest = ManagedAttributeFactory.newManagedAttribute()
       .acceptedValues(new String[] {"a", "b"})
+      .description(jsonNode)
       .build();
-
+  
+  @Before
+  private void setupTest() throws JsonProcessingException{
+    
+    String json = "{ \"en\" : \"attrEn\", \"fr\" : \"attrFr\"} ";  
+    ObjectMapper objectMapper = new ObjectMapper();
+    jsonNode = objectMapper.readTree(json) ;      
+  }  
+      
   @Override
   public void testSave() {
     assertNull(managedAttributeUnderTest.getId());
@@ -28,6 +45,8 @@ public class ManagedAttributeCRUDIT extends BaseEntityCRUDIT {
     assertEquals(managedAttributeUnderTest.getId(), fetchedObjectStoreMeta.getId());
     
     assertArrayEquals(new String[] {"a", "b"}, managedAttributeUnderTest.getAcceptedValues());
+    
+    assertEquals(jsonNode, managedAttributeUnderTest.getDescription());
     
     assertNotNull(fetchedObjectStoreMeta.getCreatedDate());
   }
