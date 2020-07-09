e458219237bb0a692f638273b6938aebca4f5b35
Jkeyuk <Jonkero2@gmail.com>
2020-07-09 08:31:30 -0400

    19756_manage_attribute_uses_new_dina_layers
    Change repo for managed attribute to extend dina repo and added the needed dina service class

--- src/main/java/ca/gc/aafc/objectstore/api/respository/ManagedAttributeResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/ManagedAttributeResourceRepository.java
@@ -1,30 +1,28 @@
 package ca.gc.aafc.objectstore.api.respository;
 
-import java.util.Arrays;
-
 import org.springframework.stereotype.Repository;
 
-import ca.gc.aafc.dina.filter.RsqlFilterHandler;
-import ca.gc.aafc.dina.filter.SimpleFilterHandler;
-import ca.gc.aafc.dina.repository.JpaDtoRepository;
-import ca.gc.aafc.dina.repository.JpaResourceRepository;
-import ca.gc.aafc.dina.repository.meta.JpaMetaInformationProvider;
+import ca.gc.aafc.dina.filter.DinaFilterResolver;
+import ca.gc.aafc.dina.mapper.DinaMapper;
+import ca.gc.aafc.dina.repository.DinaRepository;
+import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.dto.ManagedAttributeDto;
+import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
+import lombok.NonNull;
 
 @Repository
-public class ManagedAttributeResourceRepository extends JpaResourceRepository<ManagedAttributeDto> {
+public class ManagedAttributeResourceRepository
+    extends DinaRepository<ManagedAttributeDto, ManagedAttribute> {
 
   public ManagedAttributeResourceRepository(
-    JpaDtoRepository dtoRepository,
-    SimpleFilterHandler simpleFilterHandler,
-    RsqlFilterHandler rsqlFilterHandler,
-    JpaMetaInformationProvider metaInformationProvider
+    @NonNull DinaService<ManagedAttribute> dinaService,
+    @NonNull DinaFilterResolver filterResolver
   ) {
     super(
+      dinaService,
+      new DinaMapper<>(ManagedAttributeDto.class),
       ManagedAttributeDto.class,
-      dtoRepository,
-      Arrays.asList(simpleFilterHandler, rsqlFilterHandler),
-      metaInformationProvider
-    );
+      ManagedAttribute.class, filterResolver);
   }
+
 }
