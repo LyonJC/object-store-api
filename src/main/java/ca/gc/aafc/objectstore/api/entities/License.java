11aa6a9641386411fcaad919f2a2892b10ece5fa
lyonj <justin.lyon@canada.ca>
2020-07-21 12:32:54 -0400

    Feature #19668 Add configuration file for list of supported licenses
    https://redmine.biodiversity.agr.gc.ca/issues/19668
    
    Adjusted licenses.yml to be a list of licenses instead of the license name being the key, instead containing the name as a part of the object. Adjusted entity accordingly and created function to find a specific Licence

--- src/main/java/ca/gc/aafc/objectstore/api/entities/License.java
+++ src/main/java/ca/gc/aafc/objectstore/api/entities/License.java
@@ -4,14 +4,24 @@ import java.util.HashMap;
 
 
 public class License {
+    private String name;
     private String url;
-    private HashMap<String, String> titles;
+    private HashMap<language, String> titles;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 
     public String getUrl() { return url; }
 
     public void setUrl(String url) { this.url = url; }
 
-    public HashMap<String, String> getTitles() { return titles; }
+    public HashMap<language, String> getTitles() { return titles; }
 
-    public void setTitles(HashMap<String, String> titles) { this.titles = titles; }
+    public void setTitles(HashMap<language, String> titles) { this.titles = titles; }
+
+    public enum language {
+        en,
+        fr
+    }
 }
--- src/main/java/ca/gc/aafc/objectstore/api/respository/LicenseReadOnlyRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/LicenseReadOnlyRepository.java
@@ -1,8 +1,30 @@
 package ca.gc.aafc.objectstore.api.respository;
 
 import ca.gc.aafc.objectstore.api.YamlPropertyLoaderFactory;
+import ca.gc.aafc.objectstore.api.entities.License;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
 
+import java.util.LinkedList;
+
+@Repository
 @PropertySource(value = "classpath:licences.yml", factory = YamlPropertyLoaderFactory.class)
 public class LicenseReadOnlyRepository {
+
+
+    private LinkedList<License> licenses;
+
+    public LinkedList<License> getLicenses(){ return licenses; }
+
+    public void setLicenses(LinkedList<License> licenses) { this.licenses = licenses; }
+
+    public License findOne(String name) {
+        for (License l: licenses){
+            if (l.getName() == name){
+                return l;
+            }
+        }
+
+    }
+
 }
