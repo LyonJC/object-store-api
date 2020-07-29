a8024d1b94497621434772481b83333a554ee74b
Christian Gendreau <cgendreau@users.noreply.github.com>
2020-07-29 09:05:30 -0400

    Bug 19937 checkstyle configs ignore indentation (#126)
    * Bug #19937 Checkstyle configs ignore indentation
    Fixed indentation config and updated checkstyle
    
    * Bug #19937 Checkstyle configs ignore indentation
    Fixed file with indentation issues

--- src/main/java/ca/gc/aafc/objectstore/api/exceptionmapping/IllegalArgumentExceptionMapper.java
+++ src/main/java/ca/gc/aafc/objectstore/api/exceptionmapping/IllegalArgumentExceptionMapper.java
@@ -12,30 +12,31 @@ import io.crnk.core.engine.http.HttpStatus;
 @Named
 public class IllegalArgumentExceptionMapper implements ExceptionMapper<IllegalArgumentException> {
 
-    private static final int HTTP_ERROR_CODE = HttpStatus.BAD_REQUEST_400;
-    private static final String ERROR_TITLE = "Bad Request";
+  private static final int HTTP_ERROR_CODE = HttpStatus.BAD_REQUEST_400;
+  private static final String ERROR_TITLE = "Bad Request";
 
-    @Override
-    public ErrorResponse toErrorResponse(IllegalArgumentException exception) {
-        ErrorData errorData = ErrorData.builder()
-            .setDetail(exception.getMessage())
-            .setTitle(ERROR_TITLE)
-            .setStatus(Integer.toString(HTTP_ERROR_CODE)).build();
-        return ErrorResponse.builder().setSingleErrorData(errorData).setStatus(HTTP_ERROR_CODE).build();
-    }
+  @Override
+  public ErrorResponse toErrorResponse(IllegalArgumentException exception) {
+    ErrorData errorData = ErrorData.builder()
+      .setDetail(exception.getMessage())
+      .setTitle(ERROR_TITLE)
+      .setStatus(Integer.toString(HTTP_ERROR_CODE))
+      .build();
+    return ErrorResponse.builder().setSingleErrorData(errorData).setStatus(HTTP_ERROR_CODE).build();
+  }
 
-    @Override
-    public IllegalArgumentException fromErrorResponse(ErrorResponse errorResponse) {
-        String errorMessage = errorResponse.getErrors()
-            .stream()
-            .map(ErrorData::getDetail)
-            .collect(Collectors.joining(System.lineSeparator()));
-        return new IllegalArgumentException(errorMessage);
-    }
+  @Override
+  public IllegalArgumentException fromErrorResponse(ErrorResponse errorResponse) {
+    String errorMessage = errorResponse.getErrors()
+      .stream()
+      .map(ErrorData::getDetail)
+      .collect(Collectors.joining(System.lineSeparator()));
+    return new IllegalArgumentException(errorMessage);
+  }
 
-    @Override
-    public boolean accepts(ErrorResponse errorResponse) {
-        return errorResponse.getHttpStatus() == HTTP_ERROR_CODE;
-    }
+  @Override
+  public boolean accepts(ErrorResponse errorResponse) {
+    return errorResponse.getHttpStatus() == HTTP_ERROR_CODE;
+  }
 
 }
